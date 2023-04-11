b4eb8dca45e3e62d4ecb2852e39c389e05c178b1
Pratiklerim <fsari1408@gmail.com>
2023-04-11 18:21:04 +0300

    11042023

---
+++ src/GrupCalismasi/Deneme.java
@@ -0,0 +1,27 @@
+package GrupCalismasi;
+
+public class Deneme {
+    String isim="Johne Doe";
+    int yas=40;
+
+    public Deneme(){
+        String isim="Olcay";
+        System.out.println("isim :" + isim);
+    }
+
+    public Deneme (String isim){
+        this.yas=30;
+        System.out.println("isim :"+ isim);
+    }
+    public Deneme (String isim, int yas){
+        this("Murat");
+        this.yas=45;
+    }
+    public void Deneme (){
+        System.out.println(isim);
+    }
+
+    public static void main(String[] args) {
+        Deneme obj1= new Deneme("Ali");
+    }
+}
